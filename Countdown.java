c09084e28829c82b03a89310e548e9798b894e52
louie <louiecasula@gmail.com>
2023-11-01 21:21:29 -0400

    Tweaked the aesthetic

--- Countdown.java
+++ Countdown.java
@@ -1,13 +1,14 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-public class Main {
+public class Countdown {
     public static void main(String[] args) {
         // Set target date
         LocalDate target = LocalDate.of(2024, 1, 18);
         // Set current date
         LocalDate current = LocalDate.now();
        // Return days until
+        System.out.println("*************************");
         System.out.println("Days until Demo Day: " + current.datesUntil(target).count());
+        System.out.println("*************************");
     }
 }
