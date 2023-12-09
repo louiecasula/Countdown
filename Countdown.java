0f9433b8151f6f7773593bf9ce89923d653b1bb4
louie <louiecasula@gmail.com>
2023-12-09 09:14:36 -0500

    Added hours, minutes, seconds. Better string formatting.

--- Countdown.java
+++ Countdown.java
@@ -6,9 +6,17 @@ public class Countdown {
         LocalDate target = LocalDate.of(2024, 1, 18);
         // Set current date
         LocalDate current = LocalDate.now();
+        // Save units in variables
+        long days = current.datesUntil(target).count();
+        long hours = days * 24;
+        long minutes = hours * 60;
+        long seconds = minutes * 60;
        // Return days until
-        System.out.println("*************************");
-        System.out.println("Days until Demo Day: " + current.datesUntil(target).count());
-        System.out.println("*************************");
+        System.out.println("***********************************");
+        System.out.printf( "Days until Demo Day:      %,9d\n", days);
+        System.out.printf( "Hours until Demo Day:     %,9d\n", hours);
+        System.out.printf( "Minutes until Demo Day:   %,9d\n", minutes);
+        System.out.printf( "Seconds until Demo Day:   %,9d\n", seconds);
+        System.out.println("***********************************");
     }
 }
